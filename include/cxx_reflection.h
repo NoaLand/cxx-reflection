be90e7243786ba135ccfb2f3898ceb23640e10d4
Edward NoaLand <edwardnoaland@gmail.com>
2024-04-10 14:39:26 +0800

    feat: implement store field info in to type

--- include/cxx_reflection.h
+++ include/cxx_reflection.h
@@ -1,4 +1,46 @@
 #ifndef CPP_PROJECT_TEMPLATE_TEMPLATE_HEADER_H
 #define CPP_PROJECT_TEMPLATE_TEMPLATE_HEADER_H
 
+#include <string>
+#include <memory>
+#include <utility>
+#include <vector>
+
+namespace refl {
+//    namespace {
+        struct field_base {
+            virtual std::string get_name() = 0;
+            virtual ~field_base() = default;
+        };
+
+        template<typename FT>
+        class _field : public field_base {
+        public:
+            _field(std::string name, const size_t& offset) : name{std::move(name)}, offset{offset} {}
+            std::string get_name() override { return name; }
+        private:
+            std::string name;
+            size_t offset;
+            using type = FT;
+        };
+//    }
+
+    template<typename T>
+    class type {
+    public:
+        template<typename... F>
+        explicit type(F... field) {
+            // and need to calculate the offset of each field
+            (fields.emplace_back(std::unique_ptr<field_base>(field.template operator()<T>())), ...);
+        }
+
+        //    private:
+        std::vector<std::unique_ptr<field_base>> fields;
+    };
+
+
+}
+
+#define refl_field(NAME) []<typename T>(){ return std::make_unique<refl::_field<decltype(T::NAME)>>(#NAME, offsetof(T, NAME)); }
+
 #endif //CPP_PROJECT_TEMPLATE_TEMPLATE_HEADER_H
--- tests/reflection_tests.cpp
+++ tests/reflection_tests.cpp
@@ -1,5 +1,31 @@
 #include "gtest/gtest.h"
 
+#include <string>
+
+#include "cxx_reflection.h"
+
+class Foo {
+public:
+    int i;
+    double d;
+};
+
+class Bar {
+public:
+    Foo foo;
+    std::string str;
+};
+
+refl::type<Foo> foo{refl_field(i), refl_field(d)};
+refl::type<Bar> bar{refl_field(foo), refl_field(str)};
+
+
 TEST(test_template, test_should_pass_when_1_is_equal_to_1) {
-    ASSERT_EQ(1, 1);
+    ASSERT_EQ(foo.fields.size(), 2);
+    ASSERT_EQ(foo.fields[0]->get_name(), "i");
+    ASSERT_EQ(foo.fields[1]->get_name(), "d");
+
+    ASSERT_EQ(bar.fields.size(), 2);
+    ASSERT_EQ(bar.fields[0]->get_name(), "foo");
+    ASSERT_EQ(bar.fields[1]->get_name(), "str");
 }
