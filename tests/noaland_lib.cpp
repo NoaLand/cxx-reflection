c7e0088c3df74ed03804d81eff49a98740fcc579
Edward NoaLand <edwardnoaland@gmail.com>
2024-04-20 10:37:36 +0800

    feat: should support vector template with is a

--- tests/noaland_lib.cpp
+++ tests/noaland_lib.cpp
@@ -2,6 +2,8 @@
 
 #include "noaland_lib.h"
 
+#include <vector>
+
 struct Foo {
     int i;
     double d;
@@ -12,6 +14,8 @@ TEST(is_a_test, should_successfully_detect_two_same_types_std) {
     ASSERT_TRUE((noaland::is_a_v<int, int>));
 
     ASSERT_TRUE((noaland::is_a_v<Foo, Foo>));
+
+    ASSERT_TRUE((noaland::is_a_v<std::vector<int>, std::vector<int>>));
 }
 
 TEST(is_a_test, should_successfully_detect_one_type_with_i_dont_care) {
