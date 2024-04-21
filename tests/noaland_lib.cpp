a18d21bacd4537dac29ea0af79f043dc23be8e37
Edward NoaLand <edwardnoaland@gmail.com>
2024-04-21 09:24:18 +0800

    feat: is_a should support consteval which means it will throw compile time error when type is not a something

--- tests/noaland_lib.cpp
+++ tests/noaland_lib.cpp
@@ -11,43 +11,43 @@ struct Foo {
 };
 
 TEST(is_a_test, should_successfully_detect_two_same_types_std) {
-    ASSERT_TRUE((noaland::is_a_v<int, int>));
+    static_assert(noaland::is_a_v<int, int>);
 
-    ASSERT_TRUE((noaland::is_a_v<Foo, Foo>));
+    static_assert(noaland::is_a_v<Foo, Foo>);
 
-    ASSERT_TRUE((noaland::is_a_v<std::vector<int>, std::vector<int>>));
+    static_assert(noaland::is_a_v<std::vector<int>, std::vector<int>>);
 }
 
 TEST(is_a_test, should_successfully_detect_one_type_with_i_dont_care) {
-    ASSERT_TRUE((noaland::is_a_v<int, noaland::i_dont_care>));
-    ASSERT_TRUE((noaland::is_a_v<noaland::i_dont_care, int>));
+    static_assert(noaland::is_a_v<int, noaland::i_dont_care>);
+    static_assert(noaland::is_a_v<noaland::i_dont_care, int>);
 
-    ASSERT_TRUE((noaland::is_a_v<Foo, noaland::i_dont_care>));
-    ASSERT_TRUE((noaland::is_a_v<noaland::i_dont_care, Foo>));
+    static_assert(noaland::is_a_v<Foo, noaland::i_dont_care>);
+    static_assert(noaland::is_a_v<noaland::i_dont_care, Foo>);
 }
 
 TEST(is_a_test, should_successfully_detect_vector_with_i_dont_care_is_a_vector) {
-    ASSERT_TRUE((noaland::is_a_v<std::vector<int>, std::vector<noaland::i_dont_care>>));
-    ASSERT_TRUE((noaland::is_a_v<std::vector<Foo>, std::vector<noaland::i_dont_care>>));
-    ASSERT_TRUE((noaland::is_a_v<std::vector<Foo*>, std::vector<noaland::i_dont_care>>));
+    static_assert(noaland::is_a_v<std::vector<int>, std::vector<noaland::i_dont_care>>);
+    static_assert(noaland::is_a_v<std::vector<Foo>, std::vector<noaland::i_dont_care>>);
+    static_assert(noaland::is_a_v<std::vector<Foo*>, std::vector<noaland::i_dont_care>>);
 }
 
 template<typename T>
 struct some_template {};
 
 TEST(is_a_test, should_successfully_detect_template_of_something_with_i_dont_care) {
-    ASSERT_TRUE((noaland::is_a_v<some_template<some_template<int>>, some_template<noaland::i_dont_care>>));
-    ASSERT_TRUE((noaland::is_a_v<some_template<some_template<int>>, some_template<some_template<noaland::i_dont_care>>>));
+    static_assert(noaland::is_a_v<some_template<some_template<int>>, some_template<noaland::i_dont_care>>);
+    static_assert(noaland::is_a_v<some_template<some_template<int>>, some_template<some_template<noaland::i_dont_care>>>);
 }
 
 template<typename... T>
 struct some_variadic_template {};
 
 TEST(is_a_test, should_successfully_detect_template_of_variadic_param_with_i_dont_care) {
-    ASSERT_TRUE((noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<int, float, noaland::i_dont_care>>));
-    ASSERT_TRUE((noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<noaland::i_dont_care, float, some_variadic_template<double>>>));
+    static_assert(noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<int, float, noaland::i_dont_care>>);
+    static_assert(noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<noaland::i_dont_care, float, some_variadic_template<double>>>);
 }
 
 TEST(is_a_test, should_get_false_when_two_types_are_different) {
-    ASSERT_FALSE((noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<noaland::i_dont_care, double, some_variadic_template<double>>>));
+    static_assert(!noaland::is_a_v<some_variadic_template<int, float, some_variadic_template<double>>, some_variadic_template<noaland::i_dont_care, double, some_variadic_template<double>>>);
 }
